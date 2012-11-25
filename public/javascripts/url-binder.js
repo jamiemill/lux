dbe96f2f4c0f4d7e1353e2a63f7d32db8fa86e59
Jamie Mill <jamiermill@gmail.com>
2012-11-25 21:53:23 +0000

    Slight tidyup of url binding

--- public/javascripts/url-binder.js
+++ public/javascripts/url-binder.js
@@ -1,17 +1,18 @@
 (function($) {
 
     var UrlBinder = function(options) {
+
         this._presenter = options.presenter;
         this._$baseEl = $(options.baseEl);
+
         var slideFromURL = jQuery.bbq.getState('slide', true);
         if (slideFromURL) {
             this._presenter.showSlide(slideFromURL - 1);
         }
+
         var self = this;
         $(window).bind('hashchange', function() {self._onHashChange();});
-        this._$baseEl.bind('slidechange', function(e, number) {
-            jQuery.bbq.pushState({slide: number + 1});
-        });
+        this._$baseEl.bind('slidechange', this._onSlideChange);
     };
 
     $.extend(UrlBinder.prototype, {
@@ -25,6 +26,10 @@
             if (slide) {
                 this._presenter.showSlide(slide - 1);
             }
+        },
+
+        _onSlideChange: function(e, number) {
+            jQuery.bbq.pushState({slide: number + 1});
         }
 
     });
