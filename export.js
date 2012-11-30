bf7352534efa1ce42123fcfe551c652f2283be57
Jamie Mill <jamiermill@gmail.com>
2012-11-30 16:51:12 +0000

    Started an export script and made title tag dynamic

---
+++ export.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var jade = require('jade');
+var fs = require('fs');
+
+var publicDir = path.join(__dirname, 'public');
+var contentFile = 'views/index.jade';
+var content = jade.compile(fs.readFileSync(contentFile, 'utf-8'), {filename: contentFile})({title: 'Presentation'});
+
+console.log(content);
