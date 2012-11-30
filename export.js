ebbd45626d6e7b7eb71b2d1b745002c583a9681f
Jamie Mill <jamiermill@gmail.com>
2012-11-30 17:40:28 +0000

    Export now works

--- export.js
+++ export.js
@@ -1,10 +1,13 @@
 var path = require('path');
 var jade = require('jade');
 var fs = require('fs');
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { console.log(stdout); }
 
-var publicDir = path.join(__dirname, 'public');
+// Create index.html from layout and presentation content
 var contentFile = 'views/index.jade';
 var content = jade.compile(fs.readFileSync(contentFile, 'utf-8'), {filename: contentFile})();
-
 fs.writeFileSync('export/index.html', content);
 
+// Copy stylesheet and JS files
+exec("cp -R public/* export/", puts);
