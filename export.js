b59005ffa77b868bdd63f8cf4f3336a3f60a37ec
Jamie Mill <jamiermill@gmail.com>
2012-11-30 17:11:35 +0000

    Made export write index file

--- export.js
+++ export.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 
 var publicDir = path.join(__dirname, 'public');
 var contentFile = 'views/index.jade';
-var content = jade.compile(fs.readFileSync(contentFile, 'utf-8'), {filename: contentFile})({title: 'Presentation'});
+var content = jade.compile(fs.readFileSync(contentFile, 'utf-8'), {filename: contentFile})();
 
-console.log(content);
+fs.writeFileSync('export/index.html', content);
+
